d378b1955baf422b20cfb9bfbafd11ac21125253
jax <anobaka@qq.com>
2023-11-10 18:38:36 +0800

    add built-in constants

--- Bakabase.Infrastructures/AppSharedResource.cs
+++ Bakabase.Infrastructures/AppSharedResource.cs
@@ -8,6 +8,10 @@ namespace Bakabase.Infrastructures
 {
     public class AppSharedResource
     {
-     
+        public const string App_Exit = nameof(App_Exit);
+        public const string App_Minimize = nameof(App_Minimize);
+        public const string App_RememberMe = nameof(App_RememberMe);
+        public const string App_TipOnExit = nameof(App_TipOnExit);
+
     }
 }
