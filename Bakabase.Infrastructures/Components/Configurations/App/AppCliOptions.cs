329f00a828b50c6647ac57303ffa93e74c8fcaa4
Jax Lecter <anobaka@qq.com>
2025-04-17 14:45:02 +0800

    upd

--- Bakabase.Infrastructures/Components/Configurations/App/AppCliOptions.cs
+++ Bakabase.Infrastructures/Components/Configurations/App/AppCliOptions.cs
@@ -5,5 +5,5 @@ namespace Bakabase.Infrastructures.Components.Configurations.App;
 public record AppCliOptions
 {
     [Option("port", Required = false, HelpText = "Application will discover available ports from start port number.", Default = 34567)]
-    public int? StartPort { get; set; }
+    public int StartPort { get; set; }
 }
