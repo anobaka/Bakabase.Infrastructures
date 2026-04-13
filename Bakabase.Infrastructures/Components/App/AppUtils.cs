9eb5351f33181ef5a86b1014fc1df23222af0aed
anobaka <anobaka@qq.com>
2026-04-13 11:01:30 +0800

    fix: ns for AppContext

--- Bakabase.Infrastructures/Components/App/AppUtils.cs
+++ Bakabase.Infrastructures/Components/App/AppUtils.cs
@@ -16,7 +16,7 @@ namespace Bakabase.Infrastructures.Components.App
         public static IHostBuilder CreateAppHostBuilder<TStartup>(params string[] args) where TStartup : class
         {
             return Host.CreateDefaultBuilder(args)
-                .UseContentRoot(AppContext.BaseDirectory)
+                .UseContentRoot(System.AppContext.BaseDirectory)
                 .ConfigureServices(sc =>
                 {
                     sc.AddSimpleLogging();
